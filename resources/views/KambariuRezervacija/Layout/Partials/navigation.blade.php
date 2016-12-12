5cc254c2c546a0943fecc017bed4c7d01e1e3818
sandr4 <sandra.klezyte4@gmail.com>
2016-12-12 22:55:15 +0200

    about and contacts removed from main menu

--- resources/views/KambariuRezervacija/Layout/Partials/navigation.blade.php
+++ resources/views/KambariuRezervacija/Layout/Partials/navigation.blade.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">&#9734;&#9733;&#9733;&#9733;Hotel&#9733;&#9733;&#9733&#9734;</a>
+                <a class="navbar-brand" href="{{ url('/') }}">&#9734;&#9733;&#9733;&#9733;Hotel&#9733;&#9733;&#9733&#9734;</a>
                
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
@@ -22,12 +22,6 @@
                         <a href="{{ url('rooms') }}">Kambariai</a>
                     </li>
                     @endif
-                   <li>
-                        <a href="#">Kontaktai</a>
-                    </li>
-                    <li>
-                        <a href="#">Apie</a>
-                    </li>
                 </ul>
                 <!-- /. Login form -->
                 @if(!Auth::check())
